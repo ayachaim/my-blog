8572dfb1c23f5100894a7f3572109e47e3edd519
wangning136876 <376885153@qq.com>
2019-02-05 21:21:12 +0800

    updata

--- js/message.js
+++ js/message.js
@@ -59,7 +59,7 @@
     saveMessage: function(){
       let myForm = this.form
       let content = myForm.querySelector('input[name=content]').value
-      let name = myForm.querySelector('input[name=name]').value
+      let name = myForm.querySelector('input[name=name]').value 
       this.model.save(name, content).then(function(object) {
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`
